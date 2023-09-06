89a163b330768c5926813e7570fb5f0d40ee69eb
shivacharan22 <orugantishivacharan@gmail.com>
2023-09-06 00:38:16 -0400

    .

---
+++ cmd.sh
@@ -0,0 +1,4 @@
+!pip3 install timm
+!pip3 install \
+    --extra-index-url=https://pypi.nvidia.com \
+    cudf-cu12 dask-cudf-cu12 cuml-cu12 cugraph-cu12 cuspatial-cu12 cuproj-cu12 cuxfilter-cu12 cucim
